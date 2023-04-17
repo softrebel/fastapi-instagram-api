127bbbddff5ff5e534f0e370b39d3308d451d976
Mohammadreza Shaghouzi <sh.mohammad66@yahoo.com>
2023-04-17 18:15:52 +0430

    load environment variables in main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from typing import Union
 from fastapi import FastAPI
 import uvicorn
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+# import environment variables
+dotenv_path = Path('.env.local')
+load_dotenv(dotenv_path=dotenv_path)
+
+
 
 app = FastAPI()
-
 @app.get('/')
 def root():
     return {"Hello":"World"}
