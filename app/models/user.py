d82d40ebc6ea6984dd136dee16a05bf59d6456de
Mohammadreza Shaghouzi <sh.mohammad66@yahoo.com>
2023-04-19 15:10:57 +0430

    customize models to support authentication

--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel,Field, EmailStr
 from bson import ObjectId
-from typing import Optional, List
+from typing import Optional, List, Union
 from app.utils.custom_types import PyObjectId
 
 class UserBase(BaseModel):
+    fullname: Optional[str] = Field(...)
     username: str = Field(...)
-    password: str = Field(...)
+    hashed_password: str = Field(...)
+    disabled: Union[bool, None] = None
 
 
     class Config:
@@ -14,23 +16,43 @@ class UserBase(BaseModel):
         json_encoders = {ObjectId: str}
         schema_extra = {
             "example": {
+                "fullname":"test",
                 "username": "Jane Doe",
-                "password": "",
+                "hashed_password": "",
             }
         }
 
-
-class UserInput(UserBase):
-    account: Optional[str]
+class UserCreating(BaseModel):
+    fullname: Optional[str]
+    username: Optional[str]
     password: Optional[str]
+    disabled: Optional[bool]
 
     class Config:
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
         schema_extra = {
             "example": {
+                "fullname":"test",
                 "username": "Jane Doe",
                 "password": "",
+            }
+        }
+
+class UserInput(UserBase):
+    fullname: Optional[str]
+    username: Optional[str]
+    hashed_password: Optional[str]
+    disabled: Optional[bool]
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "fullname":"test",
+                "username": "Jane Doe",
+                "hashed_password": "",
             }
         }
 
@@ -42,7 +64,35 @@ class User(UserBase):
         json_encoders = {ObjectId: str}
         schema_extra = {
             "example": {
+                "fullname":"test",
+                "id":{
+                    "$oid": "643e78087beae247272083d2"
+                },
                 "username": "Jane Doe",
-                "password": "",
+                "hashed_password": "",
+            }
+        }
+
+
+class UserViewModel(BaseModel):
+    id: Optional[PyObjectId] = Field(default_factory=PyObjectId, alias="_id")
+    username: Optional[str]
+    disabled: Optional[bool]
+    fullname: Optional[str]
+    disabled: Optional[bool]
+
+
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "fullname":"test",
+                "id":{
+                    "$oid": "643e78087beae247272083d2"
+                },
+                "username": "Jane Doe",
             }
         }
