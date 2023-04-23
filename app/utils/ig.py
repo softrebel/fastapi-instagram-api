bbf56443bbc9e9688038b45540c684d7e6f92dab
softrebel <sh.mohammad66@gmail.com>
2023-04-23 05:29:31 +0430

    add get info and get follower

--- app/utils/ig.py
+++ app/utils/ig.py
@@ -1,8 +1,12 @@
-import os,requests
+import os
+import requests
 from datetime import datetime
 import json
+
+
 class LoginFailedException(Exception):
     pass
+
 
 class InstagramHandler():
 
@@ -11,12 +15,13 @@ class InstagramHandler():
         self.session_id = None
         self.config_link = 'https://www.instagram.com/data/shared_data/'
         self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
             "X-Xequested-Xith": "XMLHttpRequest",
             "Referer": "https://www.instagram.com/",
             "Content-Type": "application/x-www-form-urlencoded",
             "Host": "www.instagram.com",
             "Origin": "https://www.instagram.com",
+            'x-ig-app-id': '936619743392459',
 
         }
 
@@ -30,28 +35,31 @@ class InstagramHandler():
             'stopDeletionNonce': "",
             'trustedDeviceRecords': "{}"
         }
-        self.cookies={}
+        self.cookies = {}
 
-        self.link_info='https://www.instagram.com/api/v1/users/web_profile_info/?username={username}&='
+        self.link_info = 'https://www.instagram.com/api/v1/users/web_profile_info/?username={username}&='
 
-    def login(self,username, password):
+        self.link_follower = 'https://www.instagram.com/api/v1/friendships/{user_id}/followers/?count=12&search_surface=follow_list_page'
+
+    def login(self, username, password):
         time = int(datetime.now().timestamp())
         self.get_csrf_token()
 
-        pyaload=self.payload
-        pyaload['username']=username
-        pyaload['enc_password']=pyaload['enc_password'].format(password=password,time=time)
+        pyaload = self.payload
+        pyaload['username'] = username
+        pyaload['enc_password'] = pyaload['enc_password'].format(
+            password=password, time=time)
 
-        headers=self.headers
-        headers['X-CSRFToken']=self.csrf_token
+        headers = self.headers
+        headers['X-CSRFToken'] = self.csrf_token
         arugments = {'headers': headers}
         if self.is_proxy_enabled:
             arugments['proxies'] = {
                 'http': os.getenv("PROXY_HTTP"),
                 'https': os.getenv("PROXY_HTTPS"),
             }
-        arugments['data']=pyaload
-        response = requests.post(self.login_url,**arugments)
+        arugments['data'] = pyaload
+        response = requests.post(self.login_url, **arugments)
         if response.status_code != 200:
             raise Exception("could not login")
         response_json = json.loads(response.text)
@@ -63,9 +71,10 @@ class InstagramHandler():
             print("csrf_token: ", self.csrf_token)
             self.session_id = cookie_jar['sessionid']
             print("session_id: ", self.session_id)
-            self.cookies=response.cookies
+            self.cookies = response.cookies
         else:
-            raise LoginFailedException("login information is incorrect", response.text)
+            raise LoginFailedException(
+                "login information is incorrect", response.text)
 
     def get_csrf_token(self):
         arugments = {'headers': self.headers}
@@ -76,23 +85,37 @@ class InstagramHandler():
             }
 
         response = requests.get(self.config_link, **arugments)
-        config=json.loads(response.text)
+        config = json.loads(response.text)
         csrf = config['config']['csrf_token']
         self.csrf_token = csrf
 
+    def get_user_info(self, username):
+        self.get_csrf_token()
+        headers = self.headers
+        headers['X-CSRFToken'] = self.csrf_token
+        arugments = {'headers': headers}
 
-    def get_user_info(self,username):
-        arugments = {'headers': self.headers}
         if self.is_proxy_enabled:
             arugments['proxies'] = {
                 'http': os.getenv("PROXY_HTTP"),
                 'https': os.getenv("PROXY_HTTPS"),
             }
 
-
-        response = requests.get(self.link_info,cookies=self.cookies, **arugments)
-        info=json.loads(response.text)
+        link = self.link_info.format(username=username)
+        response = requests.get(link, cookies=self.cookies, **arugments)
+        info = json.loads(response.text)
+        response.close()
         return info
-    def get_followers(self,username):
-        pass
 
+    def get_followers(self, username):
+
+        info = self.get_user_info(username)
+        if not 'data' in info or 'user' not in info['data']:
+            raise Exception('data is not available')
+        id = info['data']['user']['id']
+        url = self.link_follower.format(user_id=id)
+        response = requests.get(
+            url, cookies=self.cookies, verify=False)
+        res = json.loads(response.text)
+        users = [{"username": item['username']} for item in res['users']]
+        return users
